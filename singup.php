d98a30162fbd4590d4992a5eb9ff94e5f8633cba
binhkdc <ledangbinh18122002@gmail.com>
2021-03-19 21:52:19 +0700

    First commit Signed-off-by: binhkdc <ledangbinh18122002@gmail.com>

---
+++ singup.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="js/style.js" type="text/css">
+    <link rel="stylesheet" href="bootstrap-4.5/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="bootstrap-4.5/js/bootstrap.min.js">
+</head>
+<body>
+
+</body>
+</html>
